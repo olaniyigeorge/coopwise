b6428a193a461649168da76c1de9f5217b1aa6fa
olaniyigeorge <olaniyigeorge77@gmail.com>
2025-06-02 23:35:04 +0100

    Get all users

--- api_backend/app/core/middleware.py
+++ api_backend/app/core/middleware.py
@@ -6,7 +6,7 @@ async def app_middleware(request: Request, call_next):
 
     start_time = time.time()
     response = await call_next(request)
-    process_time = (time.time() - start_time) * 1000
+    req_process_time = round((time.time() - start_time) * 1000, 3)  # in ms
 
     # if request.url == "xxx" ---> implement authorization or check auth
 
@@ -14,7 +14,7 @@ async def app_middleware(request: Request, call_next):
         "url": request.url.path,
         "method": request.method,
         "status_code": response.status_code,
-        "req_process_time": process_time
+        "req_process_time": f"{req_process_time}ms "
     }
     logger.info(log_dict, extra=log_dict)
     return response
