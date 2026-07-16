5e445e238cc84989404b900021aa14eb6a064a26
olaniyigeorge <olaniyigeorge77@gmail.com>
2026-07-16 16:06:46 +0100

    refactor(users): update user domain dependencies

--- apps/backend/src/domains/users/dependencies.py
+++ apps/backend/src/domains/users/dependencies.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 from fastapi import Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from src.infra.storage.cloudinary_storage import CloudinaryStorage
 from src.domains.users.infra.sqlalchemy_user_repository import SqlAlchemyUserRepository
 from src.domains.users.service import UserService
 from src.infra.db.dependencies import get_async_db_session
@@ -11,4 +12,6 @@ from src.infra.db.dependencies import get_async_db_session
 def get_user_service(
     db: AsyncSession = Depends(get_async_db_session),
 ) -> UserService:
-    return UserService(user_repo=SqlAlchemyUserRepository(db))
+    return UserService(
+        user_repo=SqlAlchemyUserRepository(db)
+    )
